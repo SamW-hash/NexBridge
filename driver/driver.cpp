87aed43470d44376628f69138a49d8a78a87af37
SamW-hash <samvincentwilliams@gmail.com>
2026-05-29 16:32:57 -0700

    Rolled-back many custom termios flags and replaced with raw instantiation. Telescope is now responding to raw commands

--- driver/driver.cpp
+++ driver/driver.cpp
@@ -24,26 +24,17 @@ int main() {
         return 1;
     }
 
+    cfmakeraw(&tty);
     // NexStar serial is 8N1 and communicates at a 9600 baudrate
     cfsetispeed(&tty, B9600);
     cfsetospeed(&tty, B9600);
 
-    tty.c_cflag &= ~PARENB; // Disable parity
-    tty.c_cflag &= ~CSTOPB; // One stop bit
-    tty.c_cflag &= ~CSIZE; // Clear data-size setting
-    tty.c_cflag |= CS8; // Sets data size
-    tty.c_cflag |= CREAD | CLOCAL; // Enable reading and ignore modem control
-    tty.c_lflag &= ~ICANON; // Disable canonical mode (Responses end with #)
-    tty.c_cflag &= ~ECHO; 
-    tty.c_cflag &= ~ECHOE;
-    tty.c_cflag &= ~ECHONL;
-    tty.c_cflag &= ~ISIG; // Disable terminal signals
-    tty.c_iflag &= ~(IXON | IXOFF | IXANY); // Disable flow-control commands
-    tty.c_iflag &= ~(ICRNL | INLCR); // Disable carriage returns/newlines
-    tty.c_oflag &= ~OPOST; // Disable output post-processing
+    tty.c_cflag |= CLOCAL | CREAD;
+    tty.c_cflag &= ~CRTSCTS; // disable hardware flow control
+
 
     tty.c_cc[VMIN] = 0; // Minimum number of characters to read
-    tty.c_cc[VTIME] = 20; // Maximum wait time
+    tty.c_cc[VTIME] = 50; // Maximum wait time
 
     if (tcsetattr(serialPort, TCSANOW, &tty) !=0) {
         std::cerr << "Failed to set serial settings: "
@@ -59,6 +50,7 @@ int main() {
                                   e = precise RA/DEC
                                                       */
     int bytesWritten = write(serialPort, command.c_str(), command.size());
+    tcdrain(serialPort);
     if (bytesWritten < 0) {
         std::cerr << "Failed to write command: "
                   << std::strerror(errno) << '\n';
@@ -90,6 +82,6 @@ int main() {
 
     std::cout << "Raw response: " << response << '\n';
     close(serialPort);
-    
+
     return 0;
 }
